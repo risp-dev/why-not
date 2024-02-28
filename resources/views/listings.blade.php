b513a50611d1d7f74efdd31d7ed4e92b6328b40b
Rimas <119005269+risp-dev@users.noreply.github.com>
2024-02-28 20:52:04 +0200

    listings updated

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -22,9 +22,9 @@
             />
             <div>
                 <h3 class="text-2xl">
-                    <a href="show.html">{{$listing['title']}}</a>
+                    <a href="show.html">{{$listing->title}}</a>
                 </h3>
-                <div class="text-xl font-bold mb-4">{{$listing['description']}}</div>
+                <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                 <ul class="flex">
                     <li
                         class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
@@ -48,8 +48,8 @@
                     </li>
                 </ul>
                 <div class="text-lg mt-4">
-                    <i class="fa-solid fa-location-dot"></i> Boston,
-                    MA
+                    <i class="fa-solid fa-location-dot"></i> 
+                    {{$listing->location}}
                 </div>
             </div>
         </div>
